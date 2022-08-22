e0e72e3fd6b60bec79421e2d81890297d57b913e
Shadows Go <shadows-go@pm.me>
2022-08-22 00:25:06 -0400

    Keys..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  supabaseUrl: "https://rtcqjckbjxdmdwjnqucz.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0Y3FqY2tianhkbWR3am5xdWN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjA3ODc4MjksImV4cCI6MTk3NjM2MzgyOX0.mW1M7JIYgXriUCsYS74aTNNeP2r_taCjTFlgWVUTAQ4",
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   supabaseUrl: "https://rtcqjckbjxdmdwjnqucz.supabase.co",
-  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0Y3FqY2tianhkbWR3am5xdWN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjA3ODc4MjksImV4cCI6MTk3NjM2MzgyOX0.mW1M7JIYgXriUCsYS74aTNNeP2r_taCjTFlgWVUTAQ4"
-};
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0Y3FqY2tianhkbWR3am5xdWN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjA3ODc4MjksImV4cCI6MTk3NjM2MzgyOX0.mW1M7JIYgXriUCsYS74aTNNeP2r_taCjTFlgWVUTAQ4",
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
